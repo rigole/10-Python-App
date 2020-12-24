a14f552bedc64c40d96dfce7b4b88e2f45aa5a4c
rigole <foplacide@gmail.com>
2020-12-25 00:16:36 +0100

    taking into account bad word

--- English-Dictionary/dico.py
+++ English-Dictionary/dico.py
@@ -2,7 +2,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
-    return data[w]
+    if w in data:
+       return data[w]
+    else:
+        return "The word doesn't exist. Please Check-it"
 
 word = input("Entrer the word :")
 
