8dfa8e64bc46fae1ad83d5960763d9b46fe9a7fb
rigole <foplacide@gmail.com>
2020-12-27 16:53:45 +0100

    Get closer match + recommendation

--- English-Dictionary/dico.py
+++ English-Dictionary/dico.py
@@ -1,9 +1,13 @@
 import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
+    w = w.lower() #to check cqse because the json file doesn't capital letters
     if w in data:
        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, get_close_matches(w, data.keys()))[0]
     else:
         return "The word doesn't exist. Please Check-it"
 
