18f1d1ce1027cf2074a499eb09af506633b53294
rigole <foplacide@gmail.com>
2021-01-09 22:03:15 +0100

    adding multiples point

--- Webmaps/map1.py
+++ Webmaps/map1.py
@@ -1,4 +1,5 @@
 import folium
 map = folium.Map(location=[2.40, 10], zoom_start=6, tiles="Mapbox Bright")
-map.add_child(folium.Marker(location=[4.0548443, 9.7294843],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for coordinates in [[4.0548443, 9.7294843],[5.0548443,11.7294843]]:
+    map.add_child(folium.Marker(location=coordinates,popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 map.save("Map.html")
