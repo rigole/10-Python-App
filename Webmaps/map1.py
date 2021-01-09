88fec73912004c5b3561e2e328c73c11200b726b
rigole <foplacide@gmail.com>
2021-01-09 22:31:14 +0100

    adding multiples point Using pandas and files

--- Webmaps/map1.py
+++ Webmaps/map1.py
@@ -1,5 +1,11 @@
 import folium
+import pandas
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[2.40, 10], zoom_start=6, tiles="Mapbox Bright")
-for coordinates in [[4.0548443, 9.7294843],[5.0548443,11.7294843]]:
-    map.add_child(folium.Marker(location=coordinates,popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+fg = folium.FeatureGroup(name="M map")
+for lt, ln in zip(lat, lon):
+    map.add_child(folium.Marker(location=[lt, ln],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 map.save("Map.html")
