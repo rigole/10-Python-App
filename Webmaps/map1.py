83e093d49367fb3180a3804b90d2a6829647ce97
rigole <foplacide@gmail.com>
2021-01-09 22:47:14 +0100

    popup windows

--- Webmaps/map1.py
+++ Webmaps/map1.py
@@ -3,9 +3,10 @@ import pandas
 data = pandas.read_csv("volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[2.40, 10], zoom_start=6, tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="M map")
-for lt, ln in zip(lat, lon):
-    map.add_child(folium.Marker(location=[lt, ln],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    map.add_child(folium.Marker(location=[lt, ln],popup=str(el), icon=folium.Icon(color='green')))
 map.save("Map.html")
