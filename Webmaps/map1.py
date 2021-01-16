9c363e80372f7db7596a13c6f39ad545a51eaf1f
rigole <foplacide@gmail.com>
2021-01-16 21:51:57 +0100

    colour according to population

--- Webmaps/map1.py
+++ Webmaps/map1.py
@@ -22,6 +22,8 @@ fg = folium.FeatureGroup(name="M map")
 for lt, ln, el in zip(lat, lon, elev):
     map.add_child(folium.CircleMarker(location=[lt, ln], radius = 6,popup=str(el),
                                       icon=folium.Icon(color=color_producer(el)),color = 'grey', fill_opacity=0.7))
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 map.add_child(fg)
 map.save("Map.html")
