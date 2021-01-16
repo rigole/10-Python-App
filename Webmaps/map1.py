a55f87d1c5200f50db5f5a40c911df012046480a
rigole <foplacide@gmail.com>
2021-01-16 20:57:16 +0100

    Changing the Marker in the map

--- Webmaps/map1.py
+++ Webmaps/map1.py
@@ -20,7 +20,8 @@ elev = list(data["ELEV"])
 map = folium.Map(location=[2.40, 10], zoom_start=6, tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="M map")
 for lt, ln, el in zip(lat, lon, elev):
-    map.add_child(folium.Marker(location=[lt, ln],popup=str(el), icon=folium.Icon(color=color_producer(el))))
+    map.add_child(folium.CircleMarker(location=[lt, ln], radius = 6,popup=str(el),
+                                      icon=folium.Icon(color=color_producer(el)),color = 'grey', fill_opacity=0.7))
 
 map.add_child(fg)
 map.save("Map.html")
