93664b0f8719bb21cd4bc7c822fbfc3c4a52103f
rigole <foplacide@gmail.com>
2021-01-18 01:32:55 +0100

    new project blocking website

--- Webmaps/map1.py
+++ Webmaps/map1.py
@@ -25,5 +25,7 @@ for lt, ln, el in zip(lat, lon, elev):
 fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
                             style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
+
+map.add_child(folium.LayerControl())
 map.add_child(fg)
 map.save("Map.html")
