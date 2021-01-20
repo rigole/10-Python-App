b6b485d4ba9821c701f5e42da2f342b3c167bd92
rigole <foplacide@gmail.com>
2021-01-20 23:38:02 +0100

    setting up the infinite loop

--- WebsiteBlocker/daily_blocker.py
+++ WebsiteBlocker/daily_blocker.py
@@ -4,5 +4,8 @@ hosts_path="/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com", "www.xvideos.com"]
 while True:
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+    else:
+        print("Fun hours...")
     time.sleep(5)
