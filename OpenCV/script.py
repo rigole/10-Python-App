f6c4ca1460af03a8d2de718e057645c67fa36d4a
rigole <foplacide@gmail.com>
2021-02-16 07:14:27 +0100

    Showing image and resizing

--- OpenCV/script.py
+++ OpenCV/script.py
@@ -1,1 +1,11 @@
 import  cv2
+img = cv2.imread("galaxy.jpg",1)
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized_image = cv2.resize(img,(500, 900))
+cv2.imshow("Galaxy", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
