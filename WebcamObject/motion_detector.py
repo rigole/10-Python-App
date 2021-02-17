68510e6466d3ba41b2f9337e5af89fc4371d3f26
rigole <foplacide@gmail.com>
2021-02-17 23:47:52 +0100

    delta frame + gray frame

--- WebcamObject/motion_detector.py
+++ WebcamObject/motion_detector.py
@@ -1,17 +1,21 @@
 import cv2,time
 video = cv2.VideoCapture(0)
-first_name=None
+first_frame=None
 while True:
     check, frame = video.read()
 
     #time.sleep(3)
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray= cv2.GaussianBlur(gray,(21,21),0)
 
-    if first_name is None:
-        first_name=gray
+    if first_frame is None:
+        first_frame=gray
         continue
 
+    delta_frame = cv2.absdiff(first_frame, gray)
+
     cv2.imshow("capturing", frame)
+    cv2.imshow("delata frame", delta_frame)
 
     key=cv2.waitKey(1)
     if key==ord('q'):
