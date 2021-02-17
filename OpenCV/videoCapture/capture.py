80473685ac338fe0254c0ce8bec44b88f03b5111
rigole <foplacide@gmail.com>
2021-02-17 23:33:26 +0100

    Webcam Detector

--- OpenCV/videoCapture/capture.py
+++ OpenCV/videoCapture/capture.py
@@ -1,11 +1,16 @@
 import cv2,time
 video = cv2.VideoCapture(0)
-
+first_name=None
 while True:
     check, frame = video.read()
 
     #time.sleep(3)
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    if first_name is None:
+        first_name=gray
+        continue
+
     cv2.imshow("capturing", frame)
 
     key=cv2.waitKey(1)
