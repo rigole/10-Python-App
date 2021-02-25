54490bd6596022c3323f8ff8a3ae3f750677e563
rigole <foplacide@gmail.com>
2021-02-26 00:44:13 +0100

    settng path for success files

--- Data-Collector/script.py
+++ Data-Collector/script.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method=='POST':
+        email=request.form["email_name"]
+    return render_template("success.html")
+
 if __name__ == '__main__':
     app.debug=True
     app.run()
