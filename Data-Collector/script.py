671c9c6e75c27ab35358f41e9dd5512fd95b2499
rigole <foplacide@gmail.com>
2021-03-05 09:30:19 +0100

    handling error of same email address

--- Data-Collector/script.py
+++ Data-Collector/script.py
@@ -31,10 +31,13 @@ def success():
         email=request.form["email_name"]
         height=request.form["height_name"]
         print(request.form)
-        data=Data(email,height)
-        db.session.add(data)
-        db.session.commit()
-    return render_template("success.html")
+        if(db.session.query(Data).filter(Data.email_==email).count()==0):
+            data=Data(email,height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+    return render_template('index.html',
+    text="Seems like we've got something from that email address already!")
 
 if __name__ == '__main__':
     app.debug=True
