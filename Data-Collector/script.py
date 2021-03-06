0510ebaf236a98371674e5e6e4daf083d1c0c6cc
rigole <foplacide@gmail.com>
2021-03-06 23:38:14 +0100

    showing results to user

--- Data-Collector/script.py
+++ Data-Collector/script.py
@@ -31,12 +31,15 @@ def success():
     if request.method=='POST':
         email=request.form["email_name"]
         height=request.form["height_name"]
-        send_email(email, height)
+
         if(db.session.query(Data).filter(Data.email_==email).count()==0):
             data=Data(email,height)
             db.session.add(data)
             db.session.commit()
             average_height = db.session.query(func.avg(Data.height_)).scalar()
+            average_height=round(average_height,1)
+            count=db.session.query(Data.height_).count()
+            send_email(email, height, average_height, count)
             print(average_height)
             return render_template("success.html")
     return render_template('index.html',
--- Data-Collector/send_email.py
+++ Data-Collector/send_email.py
@@ -1,12 +1,12 @@
 from email.mime.text import MIMEText
 import  smtplib
-def send_email(email, height):
+def send_email(email, height, average_height, count):
      from_email="stevenswynter@gmail.com"
      from_password="cybercrea"
      to_email=email
 
      subject="height data "
-     message="Hey there, your height is <strong>%s</strong>." % height
+     message="Hey there, your height is <strong>%s</strong>. Average height of all is %s and that is calculated out of #s of users" % (height, average_height, count)
 
      msg=MIMEText(message, 'html')
      msg['Subject']=subject
